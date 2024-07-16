3f9a8e2f77a91976688753be31f0ee04c0ecf640
Hema <hhippopotamus@163.com>
2024-07-16 09:59:04 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -55,8 +55,18 @@ def forge():
     click.echo('Done')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
